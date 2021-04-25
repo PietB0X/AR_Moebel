1bfa19429a70a296f02d951389dee56ba647abc1
PietB0X <p.berendjy1@googlemail.com>
2021-04-25 23:46:59 +0200

    Script hinzugefügt

---
+++ Assets/Prefabs/ARTapToPlaceObject.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.Experimental.XR;
+using UnityEngine.XR.ARSubsystems;
+
+public class ARTapToPlaceObject : MonoBehaviour
+{
+    public GameObject placementIndicator;
+
+    private ARSessionOrigin arOrigin;
+    private Pose placementPose;
+    private ARRaycastManager raycastManager;
+    private bool placementPoseIsValid = false;
+
+
+    void Start()
+    {
+        raycastManager = FindObjectOfType<ARRaycastManager>();
+    }
+
+    void Update()
+    {
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+    }
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementPoseIsValid)
+        {
+            placementIndicator.SetActive(true);
+            placementIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else
+        {
+            placementIndicator.SetActive(false);
+        }
+    }
+
+    private void UpdatePlacementPose()
+    {
+        var screenCenter = Camera.current.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        raycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+
+        placementPoseIsValid = hits.Count > 0;
+        if (placementPoseIsValid)
+        {
+            placementPose = hits[0].pose;
+        }
+    }
+}
